cdd3117acde62bac01ad78d39b73e6faec2103c3
Mary Gore <dm101mary@gmail.com>
2023-09-10 21:08:57 +0400

    update gallery, mobile style settings

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -35,14 +35,19 @@ const gallerySwiper = new Swiper('.gallery__swiper', {
     },
     577: {
       slidesPerView: 2,
-      slidesPerGroup: 4,
+      slidesPerGroup: 2,
 
       grid: {
         rows: 2
       }
     },
     320: {
-      slidesPerView: 1
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+
+      grid: {
+        rows: 1
+      }
     }
   }
 });
