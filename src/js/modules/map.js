1bd298fc76bd534f5767ef2b8620d8e29d7cd97b
Mary Gore <dm101mary@gmail.com>
2023-09-13 00:20:33 +0400

    update map.js

--- src/js/modules/map.js
+++ src/js/modules/map.js
@@ -8,7 +8,7 @@ function init() {
 
   const placemark = new ymaps.Placemark(center, {}, {
     iconLayout: 'default#image',
-    iconImageHref: `${app.path.rootFolder}/${app.path.buildFolder}/img/iconGeo.svg`,
+    iconImageHref: `../img/iconGeo.svg`,
     iconImageSize: [40, 55],
     iconImageOffset: [-80, -10]
   });
